3c32ebc334cf4c8fb3611fa63892563fb9868d22
MOHAMMED SHIBIN V <72612100+MohdShibin@users.noreply.github.com>
2022-07-22 18:27:37 +0530

    updated friend chat backend

--- lib/components/circular_menu_button.dart
+++ lib/components/circular_menu_button.dart
@@ -1,5 +1,7 @@
 import 'package:community_app/components/add_friend_box.dart';
 import 'package:community_app/pages/community_pages/select_members_page.dart';
+import 'package:community_app/pages/friend_chat_pages/find_friend_page.dart';
+import 'package:community_app/pages/friend_chat_pages/friend_chat_page.dart';
 import 'package:flutter/material.dart';
 import '../pages/settings_page.dart';
 import '../pages/profile_page.dart';
@@ -36,8 +38,12 @@ class _CircularMenuButtonState extends State<CircularMenuButton>
   @override
   Widget build(BuildContext context) =>
       Flow(delegate: FlowMenuDelegate(controller: controller), children: [
+        // buildButton(Icons.person_add_alt_sharp, () {
+        //   addFriend(context);
+        // }),
         buildButton(Icons.person_add_alt_sharp, () {
-          addFriend(context);
+           Navigator.push(context,
+              MaterialPageRoute(builder: (context) =>  FindFriendPage()));
         }),
         // buildButton(Icons.group_add, () {
         //   addCommunity(context);
